d34bbc2621e75527662011b7ccd7f2c80b348e46
teng.li2 <teng.li2@thoughworks.com>
2020-06-14 14:19:03 +0800

    [李腾][2020.6.14]js module homework

--- src/__test__/practice3.test.js
+++ src/__test__/practice3.test.js
@@ -1,5 +1,5 @@
-import { getPoetry } from "../practice3";
 import fetchMock from "fetch-mock";
+import { getPoetry1, getPoetry2 } from "../practice3";
 
 fetchMock.mock("https://v1.jinrishici.com/all.json", {
   content: "天清一雁远，海阔孤帆迟。",
@@ -9,6 +9,21 @@ fetchMock.mock("https://v1.jinrishici.com/all.json", {
 });
 
 test("properties on kevin and winnie expected", async () => {
-  const result = await getPoetry();
-  expect(result).toEqual(['送张舍人之江东', '李白', '天清一雁远，海阔孤帆迟。']);
+  // eslint-disable-next-line no-undef
+  const result = await getPoetry1();
+  expect(result).toEqual([
+    "送张舍人之江东",
+    "李白",
+    "天清一雁远，海阔孤帆迟。",
+  ]);
+});
+
+test("use anothor methid properties on kevin and winnie expected", async () => {
+  // eslint-disable-next-line no-undef
+  const result = await getPoetry2();
+  expect(result).toEqual([
+    "送张舍人之江东",
+    "李白",
+    "天清一雁远，海阔孤帆迟。",
+  ]);
 });
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,47 @@
-export const getPoetry
+export const getPoetry1 = async function () {
+  const result = fetch("https://v1.jinrishici.com/all.json", {
+    method: "GET",
+    headers: {
+      accept: "text/html",
+    },
+    credentials: "same-origin",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((text) => {
+      const re = [];
+      re.push(text.origin);
+      re.push(text.author);
+      re.push(text.content);
+      return re;
+    })
+    .catch((err) => console.log(err));
+
+  return result;
+};
+
+export const getPoetry2 = function () {
+  // eslint-disable-next-line consistent-return
+  const promise = new Promise((resolve, reject) => {
+    fetch("https://v1.jinrishici.com/all.json", {
+      method: "GET",
+      headers: {
+        accept: "text/html",
+      },
+      credentials: "same-origin",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((text) => {
+        const re = [];
+        re.push(text.origin);
+        re.push(text.author);
+        re.push(text.content);
+        resolve(re);
+      })
+      .catch((err) => reject(err));
+  });
+  return promise;
+};
